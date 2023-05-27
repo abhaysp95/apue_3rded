0c29a7f0bef46c11fe430d199dee45a410f27932
abhay <abhaysp95@protonmail.com>
2023-05-27 10:22:14 +0530

    feat: SIGCLD handler

---
+++ signals/fig10-6.c
@@ -0,0 +1,41 @@
+#include <apue.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static void sig_cld_handler(int);
+
+int main(void) {
+
+	pid_t pid;
+
+	// linux uses SIGCHLD in place of SIGCLD (hence, here SIGCLD will behave as SIGCHLD)
+	if (signal(SIGCLD, sig_cld_handler) == SIG_ERR) {
+		err_sys("signal error");
+	}
+	if ((pid = fork()) < 0) {
+		perror("fork() error");
+	} else if (pid == 0) {  // child
+		sleep(2);
+		_exit(0);
+	}
+
+	pause();  // parent
+	exit(0);
+}
+
+static void sig_cld_handler(int signo) {
+	pid_t pid;
+	int status;
+
+	if (signal(SIGCLD, sig_cld_handler) == SIG_ERR) {
+		perror("signal() error");
+	}
+	// pid will store child pid
+	if ((pid = wait(&status)) < 0) {  // fetch child status
+		perror("wait() error");
+	}
+
+	printf("pid (terminated child): %d\n", pid);
+}
