de448a9280d163506a7b509548b3f16086b23462
abhay <abhaysp95@protonmail.com>
2023-06-01 22:09:34 +0530

    feat: print signal mask for process

---
+++ signals/fig10-14.c
@@ -0,0 +1,32 @@
+#include <apue.h>
+#include <errno.h>
+#include <signal.h>
+
+void pr_mask(const char* str) {
+	sigset_t sigset;
+	int errno_back;
+
+	errno_back = errno;
+	if (sigprocmask(0, NULL, &sigset) == -1) {
+		err_ret("sigprocmask() error");
+	} else {
+		printf("%s", str);
+		if (sigismember(&sigset, SIGINT)) {  // 1 if set, 0 if not
+			printf(" SIGINT");
+		}
+		if (sigismember(&sigset, SIGQUIT)) {  // 1 if set, 0 if not
+			printf(" SIGQUIT");
+		}
+		if (sigismember(&sigset, SIGUSR1)) {  // 1 if set, 0 if not
+			printf(" SIGUSR1");
+		}
+		if (sigismember(&sigset, SIGUSR2)) {  // 1 if set, 0 if not
+			printf(" SIGUSR2");
+		}
+
+		// ... and so on for the rest of the signals
+		printf("\n");
+	}
+
+	errno = errno_back;
+}
