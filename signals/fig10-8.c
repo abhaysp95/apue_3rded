61c37d9a42b8539cea8cd007dd4d52ed84495935
abhay <abhaysp95@protonmail.com>
2023-06-03 22:54:30 +0530

    chore: testing out the behavior of unreliable signal()

--- signals/fig10-8.c
+++ signals/fig10-8.c
@@ -1,10 +1,13 @@
 #include <apue.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <unistd.h>
 
 static jmp_buf env_alarm;
+// static int handler_counter = 0;
 
 static void sig_alarm_handler(int signo) {
+	// handler_counter++;
 	longjmp(env_alarm, 1);
 }
 
@@ -18,5 +21,23 @@ unsigned int sleep2(unsigned int seconds) {
 		pause();
 	}
 
+	// testing something
+	/* printf("handler counter: %d\n", handler_counter);
+	printf("sleep again for 3 seconds\n");
+	alarm(3);  // this will not go to handler (thus terminate)
+	pause();
+	printf("handler counter: %d\n", handler_counter); */
+
 	return alarm(0);
 }
+
+/* int main(void) {
+	printf("slept for 5 seconds\n");
+	sleep2(5);
+	// sleep2(3);
+	// printf("slept for 3 seconds\n");
+
+	exit(0);
+} */
+
+// NOTE: the commented parts show how signal() is unreliable
