774906d234db5449d74d3d60ab6ff22fb63ce397
abhay <abhaysp95@protonmail.com>
2023-06-06 11:41:38 +0530

    chore: update if signal mask is added to the sigset of thread

--- signals/fig10-14.c
+++ signals/fig10-14.c
@@ -7,7 +7,7 @@ void pr_mask(const char* str) {
 	int errno_back;
 
 	errno_back = errno;
-	if (sigprocmask(0, NULL, &sigset) == -1) {
+	if (sigprocmask(0, NULL, &sigset) < 0) {
 		err_ret("sigprocmask() error");
 	} else {
 		printf("%s", str);
@@ -22,6 +22,9 @@ void pr_mask(const char* str) {
 		}
 		if (sigismember(&sigset, SIGUSR2)) {  // 1 if set, 0 if not
 			printf(" SIGUSR2");
+		}
+		if (sigismember(&sigset, SIGALRM)) {  // 1 if set, 0 if not
+			printf(" SIGALRM");
 		}
 
 		// ... and so on for the rest of the signals
